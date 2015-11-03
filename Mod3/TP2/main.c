44da8323b81443ef59459ba6b8cb25dd5c7df23c
gabrielspires <gabrielpires@ufmg.br>
2015-11-03 00:09:50 -0200

    .

--- Mod3/TP2/main.c
+++ Mod3/TP2/main.c
@@ -1,33 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define INF 1.0/0.0
 
+//MODELAR COMO GRAFO
+//TENTAR USAR MATRIZ
 int main(int argc, char const *argv[]){
 	int t, //num de instancias
 		n, //planetas consecutivos de uma rota
 		k, //num de planetas a serem reconquistados
-		i;
-	int *dist = NULL;
-	int printar = 1;
+		i, j, m;
+	int **dist;
+	// int printar = 0;
 	scanf("%d", &t);
 
 	while(t--){
-		printf("%d\n", t);
 		scanf("%d %d", &n, &k);
-		n++;
-		dist = (int*)malloc(n * sizeof(int));
-
-		if(printar) printf("\n    ");
+		n += 2;
+		dist = (int**) calloc(n , sizeof(int*));
 		for(i=0; i<n; i++){
-			scanf("%d", &dist[n]);
-			if(printar) printf("%d       ", dist[n]);
+			dist[i] = (int*) calloc(n , sizeof(int));
 		}
-		if(printar) printf("\nI------>");
-		if(printar) for(i=0; i<n-1; i++){
-			if(i < n-1) printf("P%d----->", i+1);
-			else printf("P%d", i+1);
-		}if(printar) printf("F\n\n");
 
+		for(i=1; i<n; i++){
+			scanf("%d", &dist[i-1][i]);
+		}
+		for(i=2; i<n; i++){
+			for(j=0; j<i-1; j++){
+				dist[j][i] += dist[i-1][i] + dist[j][i-1];
+			}
+		}
+		
+		printf("  ");
+		for(i=0; i<n; i++) printf(" %2d ", i);
+		printf("\n  +");
+		for(i=0; i<n; i++) printf("---+");
+		printf("\n");
+		for(i=0; i<n; i++){ 
+			printf("%d |", i);
+			for(j=0; j<n; j++){
+				if(dist[i][j] != 0) printf("%2d |", dist[i][j]);
+				else printf("%2d |", dist[i][j]);
+			}printf("\n  +");
+			for(m=0; m<n; m++) printf("---+");
+			printf("\n");
+		}
+		
 		if(!strcmp(argv[1], "-PD")){
 			printf("Executar PD\n");
 		}else if(!strcmp(argv[1], "-AG")){
